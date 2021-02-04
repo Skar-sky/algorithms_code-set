ee313449ff1d51444950d308727db4106a413444
Skar-sky <61345444+Skar-sky@users.noreply.github.com>
2021-02-04 15:19:27 +0800

    Candy problem

--- section1 贪心算法/test1.cpp
+++ section1 贪心算法/test1.cpp
@@ -39,4 +39,25 @@ int findContentChildren(vector<int>& g, vector<int>& s){
 }
 
 
-// 2.   
+// 2.(leet-code题号：135)一群孩子站成一排，每一个孩子有自己的评分。现在需要给这些孩子发糖果，规则是如果一个孩子的评分比自己身旁的一个孩子要高，那么这个孩子就必须得到比身旁孩子更多的糖果；所有孩子至少要有一个糖果。求解最少需要多少个糖果。
+// 解题思路：从左到右，再从右至左，通过两次比较，得到满足要求的最小序列
+int candy(vector<int>& ratings) {
+    int size = ratings.size();
+    if(size < 2){
+        return size;
+    }
+    vector<int>num(size, 1);
+    for(int i = 1;i<size;i++){
+        if(ratings[i] > ratings[i-1]){
+            num[i] = num[i-1] + 1;
+        }
+    }
+    for(int j =size-1;j>=1;j--){
+        if(ratings[j-1] > ratings[j]){
+            num[j-1] = max(num[j-1],num[j]+1);
+        }
+    }
+    return accumulate(num.begin(),num.end(),0);
+}
+
+// 3.(leet-code题号：435)给定多个区间，计算让这些区间互不重叠所需要移除区间的最少个数。起止相连不算重叠。
