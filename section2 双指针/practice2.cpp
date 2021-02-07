1d241c7b3ce3a45cec4ea5b03b82de05061d8261
Skar-sky <61345444+Skar-sky@users.noreply.github.com>
2021-02-07 17:06:26 +0800

    double pointer practice

---
+++ section2 双指针/practice2.cpp
@@ -0,0 +1,80 @@
+// 5.(leet-code题号：76)滑动窗口
+// 该题的主题思路就是利用一个滑动窗口，也即r和l两个指针来确定最短字符串，但是关于临界值的问题有面向用例编程的嫌疑
+string minWindow(string s, string t) {
+    vector<int>nums(128,0);  //计算t中每个字符的次数
+    vector<bool>flag(128,false);   //nums与flag的连用类似于哈希表，flag指示是否有该字符
+    for(int i = 0;i<t.size();i++){
+        flag[t[i]] = true;
+        nums[t[i]]++;
+    }
+    int count = 0, l = 0, min_l = 0, min_size = s.size()+1;
+    for(int r = 0;r<s.size();r++){
+        if(flag[s[r]]){
+            if(--nums[s[r]] >= 0){
+                count++;
+            }
+            //若我们已经找到完全覆盖字符串t的部分，那么我们接下来的工作就是通过右移l指针，找到最短的部分
+            while(count == t.size()){
+                if(r-l+1 <= min_size){
+                    min_l = l;
+                    min_size = r-l+1;
+                }
+                if(flag[s[l]] && ++nums[s[l]] > 0){ //关于该条件的解释，只有当我们在l右移时，右移的字符是t中所含有的，同时不是多余的字符，我们才进行计数的减一
+                    count--;
+                }
+                l++;
+            }
+        }
+    }
+    return min_size > s.size() ? "":s.substr(min_l, min_size);
+}
+
+
+
+// 6.(leet-code题号：633)平方数之和
+// 利用双指针进行计算即可，类比于前面我们介绍的有序数组之和
+bool judgeSquareSum(int c) {
+    int i = 0, j = (int)(sqrt(c));
+    long sum = 0;
+    while(i <= j){
+        sum = pow(i,2) + pow(j,2);
+        if(sum < c){
+            i++;
+        }
+        else if(sum > c){
+            j--;
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+// 8.(leet-code题号：680)验证回文字符串
+// 该题需要考虑到时间复杂度的问题，不能单纯考虑o(n^2)的算法，下面就是o(n)算法的主要步骤
+bool validPalindrome(string s) {
+    int flag=0;
+    if(Palindrome(s,0,s.size()-1)){
+        return true;
+    }
+    for(int i = 0,j = s.length()-1;i < j;i++,j--){
+        if(s[i] != s[j]){
+            return (Palindrome(s,i,j-1) || Palindrome(s,i+1,j)); //因为我们只删去一个字符，所以我们只需要考虑i到j-1，i+1到j两种情况即可
+        }
+    }
+    return true;
+}
+
+//构造判断回文字符串的函数
+bool Palindrome(string s, int low, int high){
+    for(int i = low, j = high; i<j;i++, j--){
+        if(s[i] != s[j]){
+            return false;
+        }
+    }
+    return true;
+}
+
