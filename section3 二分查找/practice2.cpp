d86b2428b191ba7825a2eef2afc58338fb511da9
Skar-sky <61345444+Skar-sky@users.noreply.github.com>
2021-02-10 17:50:16 +0800

    二分查找 commit

--- section3 二分查找/practice2.cpp
+++ section3 二分查找/practice2.cpp
@@ -24,4 +24,32 @@ int findMin(vector<int>& nums) {
 
 
 
-// 5.(leet-code题号：540)
+// 5.(leet-code题号：540)有序数组中的单一元素
+// 该题思路主要还是二分思想，根据元素奇偶数的关系以及单元素的位置进行判定
+int singleNonDuplicate(vector<int>& nums) {
+    int l = 0, r = nums.size() - 1, mid = 0;
+    while(l < r){
+        mid = (l + r)/2;
+        bool areEven = (r - mid)%2 == 0;   //判断一侧的元素个数是否为偶数
+        if(nums[mid] == nums[mid+1]){   //中间元素的相同元素在右边
+            if(areEven){        //单个元素在右侧
+                l = mid + 2;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+        else if(nums[mid] == nums[mid-1]){   //中间元素的相同元素在左边
+            if(!areEven){        //单个元素在右侧
+                l = mid + 1;
+            }
+            else{
+                r = mid - 2;
+            }
+        }
+        else{
+            return nums[mid];
+        }
+    }
+    return nums[l];
+}
