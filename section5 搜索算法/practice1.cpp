386b80dba0900aba1591503d4527303e2149c313
Skar-sky <61345444+Skar-sky@users.noreply.github.com>
2021-02-19 23:27:35 +0800

    depth-first search commit

--- section5 搜索算法/practice1.cpp
+++ section5 搜索算法/practice1.cpp
@@ -24,3 +24,35 @@ int maxAreaOfIsland(vector<vector<int>>& grid) {
     }
     return max_area;
 }
+
+// 方法二：我们利用深度优先搜索和栈来进行问题的解决
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size(), area = 0, max_area = 0, next_i = 0, next_j = 0;
+    vector<int>di{0,0,1,-1};
+    vector<int>dj{1,-1,0,0};
+    for(int i = 0;i < m;i++){
+        for(int j = 0;j < n;j++){
+            if(grid[i][j]){
+                grid[i][j] = 0;
+                area = 1;
+                stack<pair<int,int>>island;    //创建一个栈，用以存放相应的位置
+                island.push({i,j});            //将当前节点压入栈中
+                while(!island.empty()){
+                    auto [r,c] = island.top();      //取出为1的节点位置
+                    island.pop();
+                    for(int k = 0;k < 4;k++){
+                        next_i = r + di[k];
+                        next_j = c + dj[k];
+                        if(next_i>=0 && next_j>=0 && next_i<m && next_j<n && grid[next_i][next_j] == 1){   //条件判断
+                            area++;
+                            grid[next_i][next_j] = 0;
+                            island.push({next_i,next_j});  //切记要将后续的位置压入栈中，便于下一次搜索
+                        }
+                    } 
+                }
+                max_area = max(max_area, area);
+            }
+        }
+    }
+    return max_area;
+}
