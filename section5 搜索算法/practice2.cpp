7b73bb6ce2727fbd63dded45f24b59f1c986a5a1
Skar-sky <3077503204@qq.com>
2021-05-13 16:47:55 +0800

    algorithm commit

--- section5 搜索算法/practice2.cpp
+++ section5 搜索算法/practice2.cpp
@@ -82,3 +82,50 @@ public:
         return res;
     }
 };
+
+
+
+// leet-code题号：79
+// 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+// 本题实际上也是一个dfs和回溯的思想，我们需要考虑的一个问题是如何在中间进行优化，使得搜索尽早结束
+class Solution {
+public:
+    bool check(vector<vector<char>>& board, vector<vector<int>>& visited, int i, int j, string& s, int k) {
+        bool result = false;
+        if (visited[i][j] || result || board[i][j] != s[k]) {
+            return false;
+        } else if (k == s.length() - 1) {
+            return true;
+        }
+        visited[i][j] = true;
+        vector<pair<int, int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (const auto& dir: directions) {
+            int newi = i + dir.first, newj = j + dir.second;
+            if (newi >= 0 && newi < board.size() && newj >= 0 && newj < board[0].size()) {
+                if (!visited[newi][newj]) {
+                    bool flag = check(board, visited, newi, newj, s, k + 1);
+                    if (flag) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        visited[i][j] = false;
+        return result;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int h = board.size(), w = board[0].size();
+        vector<vector<int>> visited(h, vector<int>(w));
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                bool flag = check(board, visited, i, j, word, 0);
+                if (flag) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
